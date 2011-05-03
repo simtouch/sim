1fd613bd2330b0a1c96a398efd8216846d116a96
openpublix <openpublix@8c222611-9b12-f670-7bf2-15101b51dac4>
2011-05-03 01:33:38 +0000

    SwingUtil

--- SIM/src/org/sim/util/SwingUtil.java
+++ SIM/src/org/sim/util/SwingUtil.java
@@ -7,6 +7,8 @@ package org.sim.util;
 
 import java.awt.Component;
 import java.awt.Cursor;
+import java.util.List;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.RootPaneContainer;
 
 /**
@@ -27,6 +29,35 @@ public class SwingUtil {
 
     public static void setVisible(Component c, boolean value){
         c.setVisible(value);
+    }
+
+    public static DefaultComboBoxModel getComboModel(List list, boolean x){
+
+        class ComboItem{
+            @Override
+            public String toString() {
+                return "--";
+            }
+
+        }
+        int index=0, tama = list.size();
+
+        Object[] array = null;
+        if(x){
+            tama++;
+            index=1;
+            array = new Object[tama];
+            array[0]= new ComboItem();
+        }else{
+            array = new Object[tama];
+        }
+
+        for(Object o: list){
+            array[index]=o;
+        }
+        DefaultComboBoxModel model = new DefaultComboBoxModel(array);
+        return model;
+
     }
 
 }
